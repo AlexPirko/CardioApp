f413f5e876b9b611b5def2e875d6a62955c40e15
Alex Pirko <youzhny_foto@gmail.com>
2023-03-15 15:36:52 +0200

    feat: add geolocation

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function(posithion) {
+      const {latitude} = posithion.coords;
+      const {longitude} = posithion.coords;
+      console.log(`https://www.google.com.ua/maps/@${latitude},${longitude}`)
+      
+  },
+    function () {
+      alert('Невозможно получить ваше местоположение')
+    }
+  )
+}
+
