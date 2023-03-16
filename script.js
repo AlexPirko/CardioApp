dbdaacf38e9773f12efd4febc2f033096e30198f
Alex Pirko <youzhny_foto@gmail.com>
2023-03-16 16:34:10 +0200

    refactor: change to class

--- script.js
+++ script.js
@@ -5,44 +5,80 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (posithion) {
+class App {
+
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosithion();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    
+    inputType.addEventListener('change', this._toggleClimbField);
+  }
+
+  _getPosithion() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(   
+      this._loadMap.bind(this),
+      function () {
+        alert('Невозможно получить ваше местоположение')
+      })
+    }
+  }
+
+  _loadMap(posithion) {
       const {latitude} = posithion.coords;
       const {longitude} = posithion.coords;
       console.log(`https://www.google.com.ua/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude]
 
-      const map = L.map('map').setView(coords, 13);
+      this.#map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+      }).addTo(this.#map);
       
-      map.on('click', function (mapEvent) {
+      this.#map.on('click', this._showForm.bind(this));
+  }
 
-        const {lat, lng} = mapEvent.latlng;
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus(); 
+  }
 
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup'
-            })
-          )
-          .setPopupContent('Trenning')
-          .openPopup();
-      })
+  _toggleClimbField() {
+    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-    },
-    function () {
-      alert('Невозможно получить ваше местоположение')
-    }
-  )
+  _newWorkout(e) {
+    e.preventDefault();
+    
+      inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = ''
+    
+      const {lat, lng} = this.#mapEvent.latlng;
+    
+            L.marker([lat, lng])
+              .addTo(this.#map)
+              .bindPopup(
+                L.popup({
+                  autoClose: false,
+                  closeOnClick: false,
+                  className: 'running-popup'
+                })
+              )
+              .setPopupContent('Trenning')
+              .openPopup();
+  }
 }
+
+const app = new App();
+
+
